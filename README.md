6f4d4d36dc2ae548cb906c3ca1df4fe25368f15c
JeanneD4RK <mdecpr@gmail.com>
2024-07-26 15:53:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Switchbar
+
+Chocolatey package config for [https://switchbar.com/en/](Switchbar) application
+
+Switchbar lets you select which browser, browser profile, or email client to use whenever you click on a link on your Mac and PC.
+
+Current version : 16.9.1
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://cdn-2.webcatalog.io/switchbar/Switchbar%20Setup%2016.9.1.exe'
+$url64      = 'https://cdn-2.webcatalog.io/switchbar/Switchbar%20Setup%2016.9.1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'switchbar*'
+
+  checksum      = 'E96EDDA9D2679E74E849A06B26BC11C48FCA1B4E568B775AACECF846EEC94405'
+  checksumType  = 'sha256'
+  checksum64    = 'E96EDDA9D2679E74E849A06B26BC11C48FCA1B4E568B775AACECF846EEC94405'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+# prevents warning pop-up if process already exists
+Stop-Process -Name Switchbar -ErrorAction SilentlyContinue
+Install-ChocolateyPackage @packageArgs
